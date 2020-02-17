1e12f2123c97400bb2fbfd41d5388b3cdf531af9
Kanstansin Kulahin <blacktruth1991@gmail.com>
2020-02-17 21:50:37 +0300

    init: carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,15 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== "string") return false;
+  sampleActivity = Number(sampleActivity);
+  if (isNaN(sampleActivity)) return false;
+  if (sampleActivity <= 0) return false;
+  if (sampleActivity > MODERN_ACTIVITY) return false;
+  // t = (ln(MODERN_ACTIVITY/sampleActiviry))/(0.693/HALF_LIFE_PERIOD)
+  // t = HALF_LIFE_PERIOD*ln(MODERN_ACTIVITY/sampleActiviry)/0.693
+  return Math.ceil(
+    (HALF_LIFE_PERIOD * Math.log(MODERN_ACTIVITY / sampleActivity)) / 0.693
+  );
 };
