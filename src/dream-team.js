296fc1b1e2defdac5d37ff6c5f2b6a5c38e44905
Kanstantsin Kulahin <blacktruth1991@gmail.com>
2020-02-18 11:42:54 +0300

    feat: finish dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,9 +1,13 @@
 module.exports = function createDreamTeam(members) {
-  members = members.map(name =>
-    name
-      .trim()
-      .substring(0, 1)
-      .toUpperCase()
-  );
-  return members.sort().join("");
+  if (!Array.isArray(members)) return false;
+  return members
+    .map(name => {
+      if (typeof name == "string" && name.length > 0)
+        return name
+          .trim()
+          .substring(0, 1)
+          .toUpperCase();
+    })
+    .sort()
+    .join("");
 };
