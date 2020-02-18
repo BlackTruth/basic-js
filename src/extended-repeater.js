d86036fb2df48b028e0261d8cbb01722e808f32f
Kanstantsin Kulahin <blacktruth1991@gmail.com>
2020-02-18 16:54:11 +0300

    init: extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,26 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  resultAddit = [];
+  result = [];
+  let separator = "+";
+  if (options.separator !== undefined) separator = options.separator;
+  let additionSeparator = "|";
+  if (options.additionSeparator !== undefined)
+    additionSeparator = options.additionSeparator;
+  let addit = "";
+  if (typeof str != "string") str = str.toString();
+  if (options.addition !== undefined) {
+    addit = options.addition.toString();
+
+    for (let i = 1; i <= options.additionRepeatTimes; i++)
+      resultAddit.push(addit);
+
+    resultAddit = resultAddit.join(additionSeparator);
+  }
+
+  for (let i = 1; i <= options.repeatTimes; i++) {
+    result.push(str);
+    result.push(resultAddit);
+    result.push(separator);
+  }
+  return result.join("");
 };
-  
