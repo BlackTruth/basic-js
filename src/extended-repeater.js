233f652dc5aa96f245071944985b8d3cf4df3a92
Kanstantsin Kulahin <blacktruth1991@gmail.com>
2020-02-19 10:30:02 +0300

    fix: add function in extended-rapeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -4,23 +4,26 @@ module.exports = function repeater(str, options) {
   let separator = "+";
   let additionSeparator = "|";
   let addit = "";
-  if (options.separator !== undefined)
-    separator = options.separator;
+  if (options.separator !== undefined) separator = options.separator;
   if (options.additionSeparator !== undefined)
     additionSeparator = options.additionSeparator;
-  if (typeof str != "string")
-    str = str + "";
+  if (typeof str != "string") str = str + "";
   if (options.addition !== undefined) {
     addit = options.addition + "";
-    for (let i = 1; i < options.additionRepeatTimes; i++) {
-      resultAddit.push(addit);
-    }
-    resultAddit.push(addit);
-    resultAddit = resultAddit.join(additionSeparator);
+    resultAddit = glueArr(
+      addit,
+      options.additionRepeatTimes,
+      resultAddit,
+      additionSeparator
+    );
   }
-  for (let i = 1; i < options.repeatTimes; i++) {
-    result.push(str, resultAddit, separator);
-  }
-  result.push(str, resultAddit);
-  return result.join("");
+  return glueArr(str + resultAddit, options.repeatTimes, result, separator);
 };
+
+function glueArr(pushStr, repTimes, resultArr, sep) {
+  for (let i = 1; i < repTimes; i++) {
+    resultArr.push(pushStr);
+  }
+  resultArr.push(pushStr);
+  return resultArr.join(sep);
+}
